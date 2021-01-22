55f2bd02c1e48daa4cf90719743db83b797318c8
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-21 20:19:48 -0500

    commit tree 6ed2c9653dbe21f1db72ef57f66c6bfd129d77bd

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require 'pry'
+class CashRegister
+    attr_accessor(:discount, :total, :items, :price, :previous_total)
 
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+ 
+    def add_item(title, price, quantity = 1)
+        if quantity > 1
+            i=0
+            while i < quantity
+                @items << title
+                i +=1
+            end
+        else
+            @items << title
+        end
+        @previous_total = quantity*price
+        @total += @previous_total
+        @total
+    end
+    
+
+    def apply_discount
+        if @discount > 0
+            @discount = @discount/100.to_f
+        @total = @total - (@total * (@discount))
+        # binding.pry
+        return "After the discount, the total comes to $#{@total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction        
+      @total = @total - @previous_total
+
+    end
+
+
+
+
+end
